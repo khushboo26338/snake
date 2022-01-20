0b8116ca1d8e1e8eb82016d4f53817533ebecfa6
khushboo26338 <gkhushboo132@gamil.com>
2022-01-20 20:19:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from "./Board"
+import Temp from "./Temp"
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+      {/* <Temp /> */}
+    </div>
+  );
+}
+
+export default App;
